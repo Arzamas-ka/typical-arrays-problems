fc75eebd7ecab20fe2775373c1256113c122dbdb
Liudmila Arzamasova <liudmila.arzamasova@gmail.com>
2020-02-22 18:55:03 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (array.length === 0) {
+        return 0;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return Math.min(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    if (array.length === 0) {
+        return 0;
+    }
+
+    return Math.max(...array)
+};
+
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let sum = array.reduce((prev, next) => prev + next, 0)
+    return sum / array.length;
+};
